508afab5120837d1f7612c5831a8896e78fcee1f
nrdj <nriedeldj@gmail.com>
2022-01-15 08:26:05 -0300

    calculate row total

---
+++ js/application.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('tbody .shopping-row').each(function (i, ele) {
+        var price = parseFloat($(ele).children('.price').text().substring(1));
+        var qty = parseFloat($(ele).find('.qty input').val());
+
+        var subTotal = price * qty;
+    });
+});
+
