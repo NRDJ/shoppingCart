008beb37b99972c45fd39377ba8b46b8f60e19d7
nrdj <nriedeldj@gmail.com>
2022-01-16 10:35:34 -0300

    abstract the 'update total' & 'update sub total' processes into 2 functions

--- js/application.js
+++ js/application.js
@@ -1,11 +1,30 @@
-$(document).ready(function () {
+var updateSubTotal = function(ele) {
+
+    var price = parseFloat($(ele).children('.price').text().substring(1));
+    var qty = parseFloat($(ele).find('.qty input').val());
+
+    var subTotal = price * qty;
+    $(ele).children('.subTotal').html('$'+subTotal);
+
+    return subTotal;
+}
+
+
+var updateTotalPrice = function() {
+    var total = 0;
+
     $('tbody .shopping-row').each(function (i, ele) {
-        var price = parseFloat($(ele).children('.price').text().substring(1));
-        var qty = parseFloat($(ele).find('.qty input').val());
-
-        var subTotal = price * qty;
-
-        $(ele).children('.total').html(subTotal);
+        var subTotal = updateSubTotal(ele);
+        total += subTotal;
+        // total += parseFloat($(ele).find('.total').text().substring(1));
     });
-});
 
+    $('#total').html('Total Price: $'+total);
+
+    console.log(total);
+}
+
+$(document).ready(function () {
+  updateTotalPrice();
+
+});
